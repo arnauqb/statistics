d671b1f7caa4efd0a61feaba30174f480b98e585
Arnau <arnau.quera-bofarull@durham.ac.uk>
2019-11-08 11:52:33 +0000

    changed fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -8,7 +8,7 @@ def fibonacci(n):
     """
     Returns first n elements of fibonacci sequence.
     """
-    fibonacci_series = [1,1]
+    fibonacci_series = [1,0]
     for i in range(2,n):
         num1 = fibonacci_series[i-1]
         num2 = fibonacci_series[i-2]
